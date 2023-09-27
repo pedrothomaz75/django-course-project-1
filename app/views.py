f16227fceccbf8a55dabb96b417cc0022c720588
pedrothomaz75 <pedroimachida@gmail.com>
2023-09-27 02:07:42 -0300

    feat: adding templates and context dict on it

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'nome': 'Pedro'
+    }
+    return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
