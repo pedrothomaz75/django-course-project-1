a834dcff0299060ab2caada77e9579b8d0853785
pedrothomaz75 <pedroimachida@gmail.com>
2023-09-28 08:32:05 -0300

    feat: adding datas on templates

--- app/views.py
+++ app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index(request):
+    # pegando todos os dados da classe Produto
+    produtos = Produto.objects.all()
+
     context = {
-        'nome': 'Pedro'
+        'nome': 'Pedro',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod,
+    }
+    return render(request, 'produto.html', context)
